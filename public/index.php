3c412f6680398e3089988763e9ee976b46cabcca
ARTHURGOLDWAY25 <galexport08@gmail.com>
2024-08-13 00:14:41 +0200

    Add error reporting to PHP

--- public/index.php
+++ public/index.php
@@ -1,4 +1,8 @@
 <?php
 // Include or redirect to your HTML file
-include __DIR__ . '/index.html';
+include __DIR__ . '../index.html';
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 ?>
