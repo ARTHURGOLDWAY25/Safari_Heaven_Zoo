2cd97b0eff551cd6e23ba3e2f409fbe82551be1f
ARTHURGOLDWAY25 <galexport08@gmail.com>
2024-08-13 00:34:58 +0200

    Add error reporting to PHP

--- public/index.php
+++ public/index.php
@@ -1,16 +1,13 @@
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-?>
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Zoo Project</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
-    <h1>Welcome to the Zoo Project</h1>
-    <a href="/DISPLAYFLEXFOOTER/">Go to the project page</a>
+    <a href="./../contact.html">Go to the project page</a>
 </body>
 </html>
+
 
