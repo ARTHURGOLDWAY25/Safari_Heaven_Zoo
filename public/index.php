c1ec5b3b67231baf081ccfcf5aa54523bce0bb04
ARTHURGOLDWAY25 <galexport08@gmail.com>
2024-08-12 23:48:19 +0200

    Fix the path to index.html in index.php

--- public/index.php
+++ public/index.php
@@ -1,5 +1,13 @@
-<?php
-header("Location: /index.html");
-exit;
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<a href="<?php echo dirname(__DIR__); ?>/index.html">Go to the project page</a>
+</body>
+</html>
+
 
